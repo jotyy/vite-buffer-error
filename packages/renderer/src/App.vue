ac1c1ff9a52e5e4aeee4f0df03630f42124b086e
Jotyy <jotyy318@gmail.com>
2021-08-28 14:06:56 +0800

    initial commit

---
+++ packages/renderer/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <button>SEND REQUEST</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AppNavigation from "/@/components/AppNavigation.vue";
+export default defineComponent({
+  name: "App",
+  components: {
+    AppNavigation,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ packages/renderer/src/index.ts
@@ -0,0 +1,10 @@
+import {createApp} from 'vue';
+import App from '/@/App.vue';
+import router from '/@/router';
+import { Buffer } from 'buffer';
+
+window.Buffer = Buffer;
+
+createApp(App)
+  .use(router)
+  .mount('#app');
